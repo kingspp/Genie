57d972c6f500171eb75d900bae810ad3bcf2906c
Prathyush <kingspprathyush@gmail.com>
2015-07-04 12:57:32 +0530

    Update result.js

--- result.js
+++ result.js
@@ -1,38 +1,55 @@
-//v1.0.0
-
+//v1.1.0
 var casper = require('casper').create();
+var url = "http://results.vtu.ac.in/";
+var pad = "000";
+var clg = "4";
+var code = "MH";
+var year = "11";
+var branch = "EC";
+var num = 1;
+var dir = "images/";
+var ans = "001";
+var startUSN = 1;
+var endUSN = 120;
+var USN = clg+code+year+branch;
 
-casper.start('http://results.vtu.ac.in/', function() {
+casper.start(url, function() {
     this.echo(this.getTitle());
 });
 
 
-casper.waitForSelector("form[name='new']", function() {
-    this.echo("Loaded");
-	this.fill('form[name="new"]', {
-    rid: '4MH11ec075',
-});
-this.click('input[type="SUBMIT"][name="submit"]');
-    
-}, true);
 
-casper.then(function(){
-    // scrape something else
+for(var i=startUSN ; i<=endUSN ; i++){	//this.echo(clg+code+year+branch+(num++));	
+	casper.waitForSelector("form[name='new']", function() {			
+			this.fill('form[name="new"]', {
+			rid: USN+ans,
+		});
+		this.click('input[type="SUBMIT"][name="submit"]');
+		
+	}, true);
+
+	casper.then(function(){
+			// scrape something else			
+		   this.waitFor(function check() {
+			return (this.getCurrentUrl() === 'http://results.vtu.ac.in/vitavi.php');
+		},
+		function then() { // step to execute when check() is ok			
+			 casper.then(function() {
+			// capture the entire page.
+			casper.capture(dir+USN+ans+".png");
+			this.wait(1000,function(){});
+			var str = (num++).toString();
+			ans = pad.substring(0, pad.length - str.length) + str;
+			casper.echo(USN+ans);
+			casper.thenOpen(url);
+		   
+		  });
+		},
+		function timeout() { // step to execute if check has failed
+			this.echo('Failed to load to page', 'ERROR');
+		});
+	})
 	
-   this.waitFor(function check() {
-    return (this.getCurrentUrl() === 'http://results.vtu.ac.in/vitavi.php');
-},
-function then() { // step to execute when check() is ok
-    this.echo('Navigated to page 2', 'INFO');
-	 casper.then(function() {
-    // capture the entire page.
-    casper.capture("page.png"); 
-   
-  });
-},
-function timeout() { // step to execute if check has failed
-    this.echo('Failed to navigate to page 2', 'ERROR');
-});
-  })
+}
 
 casper.run();
